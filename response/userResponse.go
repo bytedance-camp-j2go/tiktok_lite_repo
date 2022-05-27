738b1fa3853b9a7b2f543e15612db655332bbe6d
evlic <krvlemon@163.com>
2022-05-27 09:55:29 +0800

    add img api, just for fun

--- response/userResponse.go
+++ response/userResponse.go
@@ -13,7 +13,32 @@ type User struct {
 	// FollowerCount int64  `json:"follower_count"` // 视频发布者粉丝数量
 	model.User
 	IsFollow bool `json:"is_follow"` // 用户是否关注这个视频发布者
+
+	// 随机图片 API
+	// Just for fun
+	Avatar          string `json:"avatar"`
+	Signature       string `json:"signature"`
+	BackgroundImage string `json:"background_image"`
+	// 随机图 API 用作背景 URL
+	// https://picsum.photos/400/200?grayscale
+	// # 随机头像API请求方式 #
+	// Method: GET
+	// # 请求地址 #
+	// https://api.sunweihu.com/api/sjtx/api.php
+	// # 参数 #
+	// lx【1.男头：a1 2.女头：b1 3.动漫：c1 4.动漫女头：c2 5.动漫男头：c3】
+	// # 返回数据 #
+	// 本API无返回数据,直接输出头像
+	// # 备注 #
+	// 后续将会增加更多类型的头像，欢迎大家投稿
+	// # 示例 #
+	// https://api.sunweihu.com/api/sjtx/api.php?lx=c1
 }
+
+const (
+	avatarAPI     = "https://api.sunweihu.com/api/sjtx/api.php"
+	backGroundAPI = "https://picsum.photos/400/200?grayscale"
+)
 
 // NewUser 查询 User 并计算是否已关注
 // uid = 被查询用户 id, u2id = 查询发起者 id
@@ -22,6 +47,9 @@ func NewUser(uid, u2id int64) (User, error) {
 		return User{
 			id,
 			isFollow(uid, u2id),
+			avatarAPI,
+			"测试签名",
+			backGroundAPI,
 		}, err
 	}
 
