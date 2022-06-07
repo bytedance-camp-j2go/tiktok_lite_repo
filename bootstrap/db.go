34e8168e853ad03f7596a69a9423fef8688b7d97
evlic <evlic.kr@icloud.com>
2022-06-07 16:44:06 +0800

    [add] conn pool

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -45,6 +45,19 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
+
 	global.DB = db
+
+	sqlDB, err := db.DB()
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	return db
 }
